8113ebe55976051c9e91fb298a2f7356a49d45c5
Matheus Martins <mathews_2010@outlook.com>
2020-06-29 11:36:31 -0300

    configuração do sequelize

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,7 @@
-import {Router} from 'express';
+import { Router } from 'express';
 
 const routes = new Router();
 
-routes.get('/', (req,res)=> {
-    return res.json("Hello Matheus")
-})
+routes.get('/', (req, res) => res.json('Hello Matheus'));
 
-export default routes
+export default routes;

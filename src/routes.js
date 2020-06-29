9569ec3496b6563c886ebcb87f578c1146a7086e
Matheus Martins <mathews_2010@outlook.com>
2020-06-29 15:25:14 -0300

    criptografando a senha do user

--- src/routes.js
+++ src/routes.js
@@ -1,21 +1,9 @@
 import { Router } from 'express';
 
-import User from './app/models/User';
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-  try {
-    const user = await User.create({
-      name: 'Matheus Martins de Oliveira',
-      email: 'matheus@gmail.com',
-      password_hash: '1728373837',
-    });
-
-    return res.json(user);
-  } catch (err) {
-    console.error('Erro de conexão com o banco de dados', err);
-  }
-});
+routes.post('/users', UserController.store);
 
 export default routes;
